65ce6ece0304867aed53c44219d37f3ca5f1ecc5
Kuzza <168814633+Kuzza111@users.noreply.github.com>
2026-02-01 10:35:09 +0300

    Refactor data handling and visualization methods
    Refactor data handling and visualization in Researcher_simplified.py. Added new methods for data analysis and improved noise handling.

--- Researcher_simplified.py
+++ Researcher_simplified.py
@@ -4,13 +4,14 @@ import random # генерация примера данных
 from dataclasses import dataclass # архитектурное решение, разделение данных и логики
 from pathlib import Path # сохранение и импорт данных
 import copy # скопировать график
+import numpy as np
 
 
 @dataclass
 class Data:
     data = None
   
-class CreateData(Data): # создание данных
+class CreateData(Data): # получение / создание данных
     def example(obj: Data, length, ran):
         data = []
 
@@ -40,44 +41,32 @@ class CollectData(Data): # импорт / сбор данных
    #csv
 
 class ShowData: # показ данных
-    def show_plot(data: Data, isOne=True, color="b"):
-        minx = 0
-        maxx = 0
-        miny = 0
-        maxy = 0
-
+    def show_plot(data: Data, color="b", block=True):
         for i in range(len(data.data["arr"])):
             el = data.data["arr"][i]
-            if el[0] < minx: minx = el[0]
-            if el[0] > maxx: maxx = el[0]
-
-            if el[1] < miny: miny = el[1]
-            if el[1] > maxy: maxy = el[1]
-
             plt.plot(el[0], el[1])
             if i > 0:
                 prev = data.data["arr"][i-1]
                 plt.plot([prev[0], el[0]], [prev[1], el[1]], f'{color}-')
-        
-        if isOne:
-            plt.xlim(minx, maxx)
-            plt.ylim(miny, maxy)
-            plt.grid(True)
-            plt.show()
-        else:
-            return minx, maxx, miny, maxy       
+        plt.grid(True)
+        plt.show(block=block)
+
+    def show_hist(obj: Data, block=True):
+        fig, ax = plt.subplots()
+        ax.hist(obj.data, bins=8, linewidth=0.5, edgecolor="white")
+        plt.show(block=block)
 
 class ClearData: # опциональный класс для сглаживания шума
-    # добавить фильтр по амплитуде шума? тип сглаживать только меньше n
-#    def value_filter(min_val, max_val):
+#    def value_filter(val, min_val, max_val): min_val <= val <= max_val
+#    def percent_filter(val, percent)
     
-    def average_value_neighbours(obj: Data, max_amp=0): # среднее между соседними значениями
+    def average_value_neighbours(obj: Data, min_amp=0, max_amp=0): # среднее между соседними значениями # переделать амплитуду в проценты?
         orig_arr = obj.data["arr"]
         new_arr = []
 
         for i in range(len(obj.data["arr"])):
             new_v = orig_arr[i][1]
-            if abs(orig_arr[i][1]) <= max_amp:
+            if abs(min_amp) <= abs(orig_arr[i][1]) <= abs(max_amp):
                 if i == 0:
                     new_v = (orig_arr[i][1] + orig_arr[i+1][1]) / 2
                 elif i == len(obj.data["arr"]) - 1:
@@ -87,36 +76,56 @@ class ClearData: # опциональный класс для сглаживан
             new_arr.append((orig_arr[i][0], new_v))
 
         obj.data["arr"] = new_arr
-    
-    # медианное значение?
 
-d = Data()
-CreateData.example(d, 100, 10)
-CreateData.add_anomalies(d)
+class Analyze: # выявление аномалий / отклонений
+    def median_deviation(obj): # возвращать только сильно отклоняющиеся данные?
+        data = copy.deepcopy(obj.data["arr"])
+        for i in range(len(data)):
+            data[i] = data[i][1]
+        data.sort()
+        obj.data = data
 
-dCleared = copy.deepcopy(d)
-
-for i in range(5):
-    ClearData.average_value_neighbours(dCleared, 10)
-
-ShowData.show_plot(d)
-ShowData.show_plot(dCleared)
+        
 
 
+    # после сглаживания находить места отклонений и затем анализировать сырые данные в этом месте? 
+    # отслеживать скорость изменения функции? (насколько отличаются соседние значения?)
+    # медианное значение? # передавать медианное значение (значения в пределах нормы / те, что чаще всего) для фильтрации?
+
+
+def main():
+    d = Data()
+    CreateData.example(d, 100, random.randint(0, 25))
+    CreateData.add_anomalies(d) # большие аномалии
+    CreateData.add_anomalies(d, 0.25, 50) # большой шум
+
+    dCleared = copy.deepcopy(d)
+    #dHist = copy.deepcopy(d)
+    #dHistCleared = copy.deepcopy(d)
+
+
+    for i in range(3):
+        ClearData.average_value_neighbours(dCleared, 0, 25)
+    #    ClearData.average_value_neighbours(dHistCleared, 0, 10)
+    #Analyze.median_deviation(dHist)
+    #Analyze.median_deviation(dHistCleared)
+
+    ShowData.show_plot(d, block=False)
+    ShowData.show_plot(dCleared, color='r', block=True)
+    #ShowData.show_hist(dHist)
+    #ShowData.show_hist(dHistCleared) # после сглаживания серединные значения размываются и может стать сложнее опредилить аномалию
+
+main()
 
 
 # остальное по мере надобности
 
 
-
-class Analyze: pass # выявление аномалий / отклонений
-    # после сглаживания находить места отклонений и затем анализировать сырые данные в этом месте? 
-
 class GeneratePredict: pass # создание предположения
 
 class CheckPredict: pass # проверка предположения
 
-# class CheckConsistency: pass
+# class CheckConsistency: pass # часть CheckPredict?
 
 class LoopManager: pass
    # цикл
